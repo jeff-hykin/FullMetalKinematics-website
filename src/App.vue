92dc32555af0b85bc4859055101711ad21b6e9af
Jeff Hykin <jeff.hykin@gmail.com>
2019-09-22 19:01:25 -0500

    add page transition effect

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
     <Nav />
-    <router-view/>
+    <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -42,5 +43,17 @@ export default {
 
 .title {
   text-align: center;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
