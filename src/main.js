aecffbe3b3f62211cf500eca8d83e08f9fa2ceb2
Jeff Hykin <jeff.hykin@gmail.com>
2019-10-02 00:06:44 -0500

    _add the project1 info

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
 import VueSimpleMarkdown from 'vue-simple-markdown'
+Vue.use(VueSimpleMarkdown)
+
 import Markdown from '@/components/Markdown'
 
-Vue.use(VueSimpleMarkdown)
-Vue.use(BootstrapVue)
+import GoodVue from 'good-vue'
+Vue.use(GoodVue)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/router.js
+++ src/router.js
@@ -29,7 +29,7 @@ export default new Router({
     {
       path: '/projects/project1',
       name: 'project1',
-      component: () => import(/* webpackChunkName: "projects" */ './views/Project1.vue')
+      component: () => import(/* webpackChunkName: "projects" */ './views/Project1/Directory.vue')
     },
   ]
 })
